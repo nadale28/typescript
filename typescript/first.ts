32af3f1a000342883048bdd68f008456bd5d6bdf
DESKTOP-3GOM72G\개리끼리 <nadale28@naver.com>
2023-12-06 23:44:32 +0900

    tuple, 타입추론, as, never, ! querySelector로 보는 typescript 장점 custom type

--- typescript/first.ts
+++ typescript/first.ts
@@ -29,5 +29,61 @@ const arr2: Array<number> = [123, 456];
 // tuple 길이 고정
 const arr3: [number, number, string] = [123, 456, 'hi'];
 
+// 근데 이런건 못막아준다...
+arr3.push(1);
+
 const h: 5 = 5;
+
+// 타입추론
+// 아래와 같이 타입 추론이 가능한걸 우리가 건드리는건 좋지 않다.
+const i = '5'; // '5'로 추론해줌
+const j:string = '5'; // 5가 더 정확하다고 볼 수 있다. 이건 괜히 우리가 string으로 이상하게 타입을 정해버린것
+
+// return 타입을 알아서 추론해준다.
+function add5 (x: number, y: number) {return x+y}
+
+const obj2: {
+    lat: number, lon:number
+} = {
+    lat: 32, lon: 127
+}
+
+function add6 (x:number, y:number): number;
+function add6 (x, y) {return x+y;}
+
+let k = 123;
+k = 'hello' as unknown as number;
+
+// never
+// 빈 배열은 never라는 type으로 되어 'hello' 같은 string 을 넣을 수 없다는데
+// 난 잘된다. 버전이 달라서 그런가
+const array = [];
+array.push('hello');
+
+const head: Element|null = document.querySelector('#head');
+if(head) {
+    head.innerHTML = 'hello world';
+
+    // 오타도 잘 잡아준다.
+    // head.innerHTMLw = 'hello world';
+}
+
+// !
+// #head는 무조건 있어! null이 올 수가 없어!
+// 근데 굳이 절대 없어! 라고 해야할까?
+// ! 는 안쓰는게 낫다. 세상에 절대가 어딨어. 사람이 실수할 수도 있지
+const head2: Element = document.querySelector('#head')!;
+
+//head3 가 null일수도 있어. 그냥 사용할거야?
+const head3 = document.querySelector('#head');
+head3.innerHTML = 'hello world';
+
+// custom type, 값 넣을때 자동완성까지 해준다.
+type World = "world" | 'hell';
+const l: World = 'world';
+type Greeting = `hello ${World}`;
+const m: Greeting = 'hello hell';
+
+function rest(...args: string[]){}
+
 
